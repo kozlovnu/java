4938bea07b62a444827bdb04c8f902d59d7b415d
Nikita <kozlov.nu@gmail.com>
2022-12-13 20:01:13 +0300

    Add task005_3

---
+++ task005_3.java
@@ -0,0 +1,56 @@
+package HomeWork;
+
+public class task005_3 {
+    // Реализовать алгоритм пирамидальной сортировки (HeapSort)
+    public static void main(String[] args) {
+        int[] arr = { 0, 25, -6, 6, 12, 80, -99, 8, -2, 27 };
+        printArray(arr);
+
+        int[] new_arr = sort(arr);
+        printArray(new_arr);
+    }
+
+    public static int[] sort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+        return arr;
+    }
+
+    public static void heapify(int arr[], int n, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+    
+}
