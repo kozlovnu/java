e5b862e1215048817ac85b31e62da03ebbb97cf9
Nikita <kozlov.nu@gmail.com>
2022-12-13 20:00:50 +0300

    Add task005_2

---
+++ task005_2.java
@@ -0,0 +1,21 @@
+package HomeWork;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class task005_2 {
+    public static void main(String[] args) {
+        String[] nameWorkers = {"Иван", "Петр", "Антон", "Сергей", "Иван",
+                "Антон", "Иван", "Антон", "Иван", "Петр", "Антон", "Сергей", "Антон", "Антон"};
+        HashMap<String, Integer> workers = new HashMap<>();
+        for (String i : nameWorkers) {
+            int newValue = workers.getOrDefault(i,0)+1;
+            workers.put(i, newValue);
+        }
+        System.out.println(workers);
+
+        workers.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .forEach(System.out::println);
+    }
+}
